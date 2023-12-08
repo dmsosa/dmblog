aa55cb85d38e3b11271a660538d88d24bbf9d2f0
Durian Sosa <dmsosa21@outlook.com>
2023-12-08 14:28:02 +0000

    Restarting

--- downloader.py
+++
@@ -1,4 +0,0 @@
-from google_images_download import google_images_download
-
-response = google_images_download.googleimagesdownload()
-response.download(arguments={"keywords":"panda", 'limit':20, 'print-urls':True})
