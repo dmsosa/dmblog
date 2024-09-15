89a7a25baa976aa92ea1bc8daf594e1fda0f2fdb
dmsosa <dmsosa21@outlook.com>
2024-09-15 17:26:22 +0100

    Unnotiger Ordner geloscht

--- backend/postgres_image/pg-entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo env vars: $POSTGRES_USER, $POSTGRES_PASSWORD
-export PGPASSWORD=$POSTGRES_PASSWORD
-
-su postgres -c 'psql -v ON_ERROR_STOP=1' <<-EOSQL
-    CREATE DATABASE dmblog;
-    GRANT ALL PRIVILEGES ON DATABASE dmblog TO $POSTGRES_USER
-EOSQL
